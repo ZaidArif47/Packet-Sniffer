815d5d192eced8dca4edb0b0eaf8801041160d89
Zaid Arif <zaid.zrf@gmail.com>
2024-07-06 20:59:29 +0530

    Unpacking IPv4 Packet & Formatting IP Address

--- packetSniffer.py
+++ packetSniffer.py
@@ -10,3 +10,15 @@ def ethernetFrame(data):
 def getMacAddr(rawMacAddr):
     mac_string = map('{:02x}'.format, rawMacAddr)
     return ':'.join(mac_string).upper()
+
+# Unpack IPv4 packet
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, srcIP, destIP = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(srcIP), ipv4(destIP), data[header_length:]
+
+# Return properly formatted IPv4 address
+def ipv4(addr):
+    return '.'.join(map(str, addr))
